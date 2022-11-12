02cf901662d5f6c65a99615b6161fb8d4a8f1e40
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-11 23:45:39 -0400

    CRUD

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const express = require('express');
+const { routerApi } = require('./routes');
 
-const app = express()
+const app = express();
+routerApi(app);
 
-const productsRouter = require('./routes/products.router');
-const categoriesRouter = require('./routes/categories.router');
-const usersRouter = require('./routes/users.router');
-
-function routerApi(app) {
-  const router = express.Router();
-  app.use('/api/v1', router);
-  router.use('/products', productsRouter);
-  router.use('/categories', categoriesRouter);
-  router.use('/users', usersRouter);
-}
-
-routerApi(app)
-app.listen(3000,()=>{
-  console.log('server up port 3000');
-})
-
-// module.exports = routerApi;
-
-// const express = require('express');
-
-// const app = express();
-// app.get('/', (req, res) => {
-//   res.send('sdfkhsdfjksdfnskjdfhkdjhdfkjshdfksdfhksafejhreraivbhy');
-// });
-
-// app.listen(3000, () => {
-//   console.log('app up');
-// });
+app.listen(3000, () => {
+  console.log('app up on port 3000');
+});
