930e09227f604a1fe7727cb399e4c342b5a05f47
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-11 23:21:39 -0400

    Separación de responsabilidades con express.Router

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-// const express = require('express');
+const express = require('express');
 
-// const productsRouter = require('./products.router');
-// const categoriesRouter = require('./categories.router');
-// const usersRouter = require('./users.router');
+const app = express()
 
-// function routerApi(app) {
-//   const router = express.Router();
-//   app.use('/api/v1', router);
-//   router.use('/products', productsRouter);
-//   router.use('/categories', categoriesRouter);
-//   router.use('/users', usersRouter);
-// }
+const productsRouter = require('./routes/products.router');
+const categoriesRouter = require('./routes/categories.router');
+const usersRouter = require('./routes/users.router');
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+}
+
+routerApi(app)
+app.listen(3000,()=>{
+  console.log('server up port 3000');
+})
 
 // module.exports = routerApi;
 
-const express = require('express');
+// const express = require('express');
 
-const app = express();
-app.get('/', (req, res) => {
-  res.send('sdfkhsdfjksdfnskjdfhkdjhdfkjshdfksdfhksafejhreraivbhy');
-});
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('sdfkhsdfjksdfnskjdfhkdjhdfkjshdfksdfhksafejhreraivbhy');
+// });
 
-app.listen(3000, () => {
-  console.log('app up');
-});
+// app.listen(3000, () => {
+//   console.log('app up');
+// });
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const faker = require('faker');
+
 const router = express.Router();
+
 router.get('/', (req, res) => {
   const products = [];
   const { size } = req.query;
   const limit = size || 10;
+
   for (let index = 0; index < limit; index++) {
     products.push({
       name: faker.commerce.productName(),
@@ -14,24 +17,30 @@ router.get('/', (req, res) => {
   }
   res.json(products);
 });
+
 router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
+
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   res.json({ id, name: 'Product 2', price: 2000 });
 });
+
 router.post('/', (req, res) => {
   const body = req.body;
   res.json({ message: 'created', data: body });
 });
+
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
   res.json({ message: 'update', data: body, id });
 });
+
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
   res.json({ message: 'deleted', id });
 });
+
 module.exports = router;
