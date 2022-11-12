51538556f2dc37e72fd18b4c948e2bfed49366b6
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-11 22:51:21 -0400

    Instalación de Express.js y tu primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('my app');
+// const express = require('express');
+
+// const productsRouter = require('./products.router');
+// const categoriesRouter = require('./categories.router');
+// const usersRouter = require('./users.router');
+
+// function routerApi(app) {
+//   const router = express.Router();
+//   app.use('/api/v1', router);
+//   router.use('/products', productsRouter);
+//   router.use('/categories', categoriesRouter);
+//   router.use('/users', usersRouter);
+// }
+
+// module.exports = routerApi;
+
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('sdfkhsdfjksdfnskjdfhkdjhdfkjshdfksdfhksafejhreraivbhy');
+});
+
+app.listen(3000, () => {
+  console.log('app up');
+});
