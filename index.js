9b43d0d0f6530bb341165f0ca4f5105bd7cb4154
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-18 10:18:51 -0400

    Problema de CORS

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+
 const { routerApi } = require('./routes');
-const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handle');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middleware/error.handle');
+
+const whileList = ['http://localhost:8080', 'https://mydomain.dev'];
+const options = {
+  origin: (origin, callback) => {
+    if (whileList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(options));
 routerApi(app);
 
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-
 
 app.listen(3000, () => {
   console.log('app up on port 3000');
