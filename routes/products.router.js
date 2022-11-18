891023670f33ceb62dfb019c11f0101a7c1640cc
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-17 23:32:07 -0400

    Async await y captura de errores

--- routes/products.router.js
+++ routes/products.router.js
@@ -4,40 +4,48 @@ const { ProductsService } = require('../services/product.service');
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.status(200).json(products);
 });
 
-router.get('/filter', (req, res) => {
+router.get('/filter', async (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
 
   res.json({ product });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { body } = req;
 
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
-  const product = service.update(id, body);
-  res.json({ message: 'update', data: product });
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const product = await service.update(id, body);
+    res.json({ message: 'update', data: product });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const deleteId = service.delete(id);
-  res.json({ message: 'deleted', deleteId });
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteId = await service.delete(id);
+    res.json({ message: 'deleted', deleteId });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 module.exports = router;
